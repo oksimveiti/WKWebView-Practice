63692058d1829e7cb320f8227222df07d551369f
Semih Cetin <semihcettin@gmail.com>
2022-09-25 21:08:16 +0300

    Initial Commit

---
+++ WKWebView Practice/WKWebView_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WKWebView_PracticeApp.swift
+//  WKWebView Practice
+//
+//  Created by Semih Cetin on 25.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct WKWebView_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
